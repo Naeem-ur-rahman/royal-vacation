2357ce65cb84d4e2f9577533f270da71809568ba
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-08-29 15:26:20 +0500

    Featured section commit

---
+++ js/carosel-swiper.js
@@ -0,0 +1,67 @@
+var swiper = new Swiper("#best-destination", {
+     spaceBetween: 30,
+     navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          color: '#ac863b'
+     }, breakpoints: {
+          600: {
+               slidesPerView: 2,
+          },
+          991: {
+               slidesPerView: 3,
+          },
+          1024: {
+               slidesPerView: 3,
+          }
+     }, autoplay: {
+          delay: 2000,
+          disableOnInteraction: true,
+     },
+});
+
+var swiper = new Swiper("#featured", {
+     spaceBetween: 30,
+     navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          color: '#ac863b'
+     }, breakpoints: {
+          600: {
+               slidesPerView: 2,
+          },
+          991: {
+               slidesPerView: 3,
+          },
+          1024: {
+               slidesPerView: 3,
+          }
+     }, autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+     },
+});
+
+var swiper = new Swiper("#featured-2", {
+     spaceBetween: 30,
+     initialSlide: 20,
+     navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          color: '#ac863b'
+     }, breakpoints: {
+          600: {
+               slidesPerView: 2,
+          },
+          991: {
+               slidesPerView: 3,
+          },
+          1024: {
+               slidesPerView: 3,
+          }
+     }, autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+          reverseDirection: true,
+     },
+});
